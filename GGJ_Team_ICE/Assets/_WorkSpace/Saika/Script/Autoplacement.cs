a7dca95ae74d61797348ee1791107aa5dd91b82e
Fubite <79892977+Fubite@users.noreply.github.com>
2022-01-29 14:03:52 +0900

    Revert "自動配置改良"
    This reverts commit f3a0324c667988908ef528c11ecc3da34d89d3b0.

--- GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Autoplacement.cs
+++ GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Autoplacement.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 public class Autoplacement : MonoBehaviour
 {
     [SerializeField] Othellodata othello;
-<<<<<<< Updated upstream
     [SerializeField] GameObject[] gameboard=new GameObject[64];
     int[] count = new int[2];
-=======
-    [SerializeField] GameObject[] gameboard;
-    int[] count= {0,0};
->>>>>>> Stashed changes
     // Start is called before the first frame update
     private void Start()
     {
         placment();
     }
-<<<<<<< Updated upstream
     public void placment()
     {
         for(int i = 0; i < gameboard.Length; i++)
@@ -26,26 +20,6 @@ public class Autoplacement : MonoBehaviour
             if (Random.Range(0, 9)%2 == 0&&count[0]<gameboard.Length/2)
             {
                 pack.reverse();
-=======
-    private void Update()
-    {
-        Debug.Log("•F" + count[0] + "”’F" + count[1]);
-
-    }
-    public void placment()
-    {
-
-        for(int i = 0; i <gameboard.Length;i++)
-        {
-            Othellodata pack = Instantiate(othello, gameboard[i].transform.position + new Vector3(0, 1, 0), transform.rotation);//¶¬Žž‚Í•
-            if (Random.Range(0, 999) % 10 <= 5 && count[1] < gameboard.Length/2)
-            {
-                pack.reverse();
-                count[1]++;//”’
-            }
-            else
-            {
->>>>>>> Stashed changes
                 count[0]++;
             }
         }
--- GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Othellodata.cs
+++ GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Othellodata.cs
@@ -4,48 +4,24 @@ using UnityEngine;
 
 public class Othellodata : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [Header("true=•\")] public bool frontback = true;
     private void Update()
-=======
-    [Header("true=•")] bool frontback = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
->>>>>>> Stashed changes
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             reverse();
-<<<<<<< Updated upstream
         }
-=======
-        }   
->>>>>>> Stashed changes
     }
     public void reverse()
     {
         if (frontback)
         {
-<<<<<<< Updated upstream
             transform.Rotate(new Vector3(180, 0, 0));
-=======
-            transform.Rotate(180, 0, 0);
->>>>>>> Stashed changes
             frontback = false;
         }
         else
         {
-<<<<<<< Updated upstream
             transform.Rotate(new Vector3(-180, 0, 0));
-=======
-            transform.Rotate(-180, 0, 0);
->>>>>>> Stashed changes
             frontback = true;
         }
     }
