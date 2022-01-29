ba9dcf8a1c552fc32c6b81caf9ed609a1e42916d
Fubite <79892977+Fubite@users.noreply.github.com>
2022-01-29 10:06:58 +0900

    Create GameMaster

--- GGJ_Team_ICE/Assets/_WorkSpace/FuchanMan/Scripts/GameMaster.cs
+++ GGJ_Team_ICE/Assets/_WorkSpace/FuchanMan/Scripts/GameMaster.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour
+public class GameMaster : SingletonMonoBehaviour<GameMaster>
 {
+    public bool winner = true;
     // Start is called before the first frame update
     void Start()
     {
