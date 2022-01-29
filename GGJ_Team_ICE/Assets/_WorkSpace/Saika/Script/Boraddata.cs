212d0b6ec2f52d7a09b5abc28e2bec3eacb82dee
sakaihodaka <sakai.hodaka@gmail.com>
2022-01-30 00:52:30 +0900

    Update Boraddata.cs

--- GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Boraddata.cs
+++ GGJ_Team_ICE/Assets/_WorkSpace/Saika/Script/Boraddata.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boraddata:MonoBehaviour
+public class Boraddata : MonoBehaviour
 {
-    public bool[,] bord =new bool[8,8];
-    public Transform[,] mass = new Transform[8,8];
-    public bool get(int x,int y)
+    public bool[,] omoteura = new bool[8, 8];//そのマスにあるオセロの裏表
+    public Transform[,] mass = new Transform[8, 8];//一マスごとの座標
+    //public bool get(int x,int y)
+    //{
+    //    return omoteura[x,y];
+    //}
+    public void set(int x, int y, bool frontback)
     {
-        return bord[x,y];
+        omoteura[x, y] = frontback;
+        Debug.Log(x + "," + y + "(" + omoteura[x, y] + ")");
     }
-    public void set(int x,int y,bool frontback)
+    private void Update()
     {
-        bord[x, y] = frontback;
+        Debug.Log(getcount(true));
     }
 
-    //1方向にひっくり返す
     void Reverse(int h, int v, int directionH, int directionV)
     {
         //確認する座標x, yを宣言
@@ -25,24 +29,23 @@ public class Boraddata:MonoBehaviour
         while (x < 8 && x >= 0 && y < 8 && y >= 0)
         {
             //自分の駒だった場合
-            if (bord[x, y] == bord[h,v])
+            if (omoteura[x, y] == omoteura[h, v])
             {
                 //ひっくり返す
                 int x2 = h + directionH, y2 = v + directionV;
                 while (!(x2 == x && y2 == y))
                 {
-                    bord[x2, y2] = bord[h,v];//set
+                    omoteura[x2, y2] = omoteura[h, v];//set
                     x2 += directionH;
                     y2 += directionV;
                 }
                 break;
             }
-        //確認座標を次に進める
-        x += directionH;
-        y += directionV;
+            //確認座標を次に進める
+            x += directionH;
+            y += directionV;
         }
     }
-    //全方向にひっくり返す
     public void ReverseAll(int h, int v)
     {
         Reverse(h, v, 1, 0);  //右方向
@@ -53,5 +56,20 @@ public class Boraddata:MonoBehaviour
         Reverse(h, v, -1, -1);//左上方向
         Reverse(h, v, 1, 1);  //右下方向
         Reverse(h, v, -1, 1); //左下方向
+    }
+    public int getcount(bool boolean)//入れた引数の数を数える関数　制限時間終了時の集計用
+    {
+        int x = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (omoteura[i, j] == boolean)
+                {
+                    x++;
+                }
+            }
+        }
+        return x;
     }
 }
